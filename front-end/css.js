d95887d8a52542ca071daebbf55b6fce89a41ec6
leoboyerbx <leoboyer.bx@gmail.com>
2018-08-28 12:26:05 +0200

    Mise à jour de css()

--- front-end/css.js
+++ front-end/css.js
@@ -4,8 +4,22 @@
  * @param {String} property Propriété
  * @param {String|Number} value Valeur
  */
- let setProperty = function (element, property, value) {
-    element.style[property] = "10px"
+let setProperty = function (element, property, value) {
+    if (value.substr(0,2) === '+=' || value.substr(0,2) === '-=') {
+        if (!value.includes('px') || !window.getComputedStyle(element).getPropertyValue(property).includes("px")) { // Cas sans px
+            console.log('Error in css() module: Unsupported property, or you need to use px values.')
+        } else {
+            let initialValue = parseFloat(window.getComputedStyle(element).getPropertyValue(property).replace(/px/, ""))
+            let valueToAdd = parseFloat(value.substr(2).replace(/px/, ""))
+            if (value.substr(0,2) === '+=') {
+                element.style[property] = (initialValue + valueToAdd) + "px"
+            } else {
+                element.style[property] = (initialValue - valueToAdd) + "px"
+            }
+        }
+    } else {
+        element.style[property] = value
+    }
  }
 
  /**
@@ -16,9 +30,9 @@
  */
 export default function (element, property, action) {
     if (arguments.length === 2) {
-        if (typeof property === 'string') {     //Cas d'une demande de propriété
+        if (typeof property === 'string') {     //Cas d'un demande de propriété
             return window.getComputedStyle(element).getPropertyValue(property)
-        } else if (typeof property === 'object' && Array.isArray(property)) {     //Cas d'une demande de propriétés multiples
+        } else if (typeof property === 'object' && Array.isArray(property)) {     //Cas d'un demande de propriétés multiples
             let styleProps = []
             property.forEach((prop) => {
                 styleProps.push([prop, window.getComputedStyle(element).getPropertyValue(prop)])
@@ -27,16 +41,16 @@ export default function (element, property, action) {
         } else if (typeof property === 'object' && !Array.isArray(property)) {
             let propsArray = Object.keys(property)
             for (let i = 0; i < propsArray.length; i++) {
-                element.style[propsArray[i]] = property[propsArray[i]]
+                setProperty(element, propsArray[i], property[propsArray[i]])
             }
         } else {
             return null
         }
     } else if (arguments.length === 3) {
         if (typeof action === 'string' || typeof action === 'number') { // Cas de modification d'une seule propriété
-            setProperty(element, property, action)
+        setProperty(element, property, action)
             return action
-        } else {
+        }    else {
             return null
         }
     } else {
